4eaef1cdef45a1dd53fe6f94772e530c77ab5a5f
Phillip Nguyen <philn0415@gmail.com>
2024-03-14 15:55:11 -0700

    completed 01

--- sql-files/01-create-tables.sql
+++ sql-files/01-create-tables.sql
@@ -1,4 +1,27 @@
 -- This ensures that SQLite enforces FOREIGN KEY constraints
 PRAGMA foreign_keys = 1;
 
--- Your code here 
+DROP TABLE IF EXISTS purchases;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS tools;
+
+CREATE TABLE tools (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    price DECIMAL(10,2),
+    department TEXT
+);
+
+CREATE TABLE customers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    phone TEXT
+);
+
+CREATE TABLE purchases (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    customer_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
+    tool_id INTEGER REFERENCES tools(id) ON DELETE CASCADE,
+    quantity INTEGER
+);
